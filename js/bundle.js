157bfde62e7357a4aad83046442fbbc983832a41
Cheleste1994 <wolverines@tut.by>
2023-04-28 16:52:09 +0300

    feat: fix function click key

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
   // eslint-disable-next-line no-undef
-  localStorage.setItem('lang', 'en');
+  if (!localStorage.getItem('lang')) {
+    localStorage.setItem('lang', 'en');
+  }
 });
 
 document.querySelector('body').classList.add('body');
@@ -76,6 +78,28 @@ function addSpan(ruOrEn, dataKey) {
   ruOrEn.appendChild(keyCaps);
 }
 
+/* start load languages */
+
+function langActive() {
+  const keyEng = document.querySelectorAll('.key__eng');
+  const keyRu = document.querySelectorAll('.key__ru');
+  if (localStorage.lang === 'en') {
+    for (let i = 0; i < keyRu.length; i += 1) {
+      keyRu[i].style.display = 'none';
+      keyEng[i].style.display = 'flex';
+      localStorage.lang = 'en';
+    }
+  } else {
+    for (let i = 0; i < keyRu.length; i += 1) {
+      keyEng[i].style.display = 'none';
+      keyRu[i].style.display = 'flex';
+      localStorage.lang = 'ru';
+    }
+  }
+}
+
+/* end load languages */
+
 async function addKeyboard(keyboard) {
   const imgHelp = './key.json';
   const res = await fetch(imgHelp);
@@ -94,6 +118,7 @@ async function addKeyboard(keyboard) {
     addSpan(addSpanRus, dataKey[i].loadRu);
     keyboard.appendChild(addDiv);
   }
+  langActive();
 }
 
 const arrEntry = [];
@@ -108,8 +133,6 @@ async function addDataKey(event) {
   const dataKey = await res.json();
   const textareaEntry = document.querySelector('#textarea');
   const eventCode = event.code;
-  // eslint-disable-next-line no-use-before-define
-  keyActive(event);
   if (dataKey[64][eventCode]) {
     arrEntry.push(dataKey[64][eventCode]);
     window.addEventListener('keydown', stopDefAction);
@@ -122,27 +145,155 @@ async function addDataKey(event) {
   textareaEntry.innerHTML = arrEntry.join('');
 }
 
+/* start key click */
+
 function keyActive(event) {
-  const keyBtn = document.querySelectorAll('.key__eng');
-  keyBtn.forEach((x) => {
-    if (x.firstChild.innerText === event.key) {
-      x.classList.add('key-active');
-    } else if (x.firstChild.innerText === 'Del' && event.key === 'Delete') {
-      x.classList.add('key-active');
+  const keyEng = document.querySelectorAll('.key__eng');
+  const keyRu = document.querySelectorAll('.key__ru');
+  for (let i = 0; i < keyEng.length; i += 1) {
+    if (keyEng[i].children[1].innerText === event.code) {
+      keyEng[i].classList.add('key-active');
+      keyRu[i].classList.add('key-active');
     }
-  });
+  }
 }
 
 function keyNoActive(event) {
-  const keyBtn = document.querySelectorAll('.key__eng');
-  keyBtn.forEach((x) => {
-    if (x.firstChild.innerText === event.key) {
-      x.classList.remove('key-active');
-    } else if (x.firstChild.innerText === 'Del' && event.key === 'Delete') {
-      x.classList.remove('key-active');
+  const keyEng = document.querySelectorAll('.key__eng');
+  const keyRu = document.querySelectorAll('.key__ru');
+  for (let i = 0; i < keyEng.length; i += 1) {
+    if (keyEng[i].children[1].innerText === event.code) {
+      keyEng[i].classList.remove('key-active');
+      keyRu[i].classList.remove('key-active');
     }
-  });
+  }
 }
 
-window.addEventListener('keydown', addDataKey);
-window.addEventListener('keyup', keyNoActive);
+/* end key click */
+
+/* start shift click */
+
+let isCaps = true;
+
+function shiftActive() {
+  const keyDown = document.querySelectorAll('.key-down');
+  const keyShift = document.querySelectorAll('.key-shift');
+  const keyCaps = document.querySelectorAll('.key-caps');
+  for (let i = 0; i < keyDown.length; i += 1) {
+    keyDown[i].style.display = 'none';
+    keyShift[i].style.display = 'block';
+    keyCaps[i].style.display = 'none';
+  }
+}
+
+function shiftNoActive(event) {
+  const keyDown = document.querySelectorAll('.key-down');
+  const keyShift = document.querySelectorAll('.key-shift');
+  const keyCaps = document.querySelectorAll('.key-caps');
+  if (event.key === 'Shift') {
+    for (let i = 0; i < keyDown.length; i += 1) {
+      keyDown[i].style.display = 'block';
+      keyShift[i].style.display = 'none';
+      keyCaps[i].style.display = 'none';
+    }
+  }
+}
+
+/* end shift click */
+
+/* start caps click */
+
+function capsActive() {
+  const keyDown = document.querySelectorAll('.key-down');
+  const keyCaps = document.querySelectorAll('.key-caps');
+  const keyShift = document.querySelectorAll('.key-shift');
+  if (isCaps) {
+    for (let i = 0; i < keyDown.length; i += 1) {
+      keyDown[i].style.display = 'none';
+      keyCaps[i].style.display = 'block';
+      keyShift[i].style.display = 'none';
+      isCaps = false;
+    }
+  } else {
+    for (let i = 0; i < keyDown.length; i += 1) {
+      keyDown[i].style.display = 'block';
+      keyCaps[i].style.display = 'none';
+      keyShift[i].style.display = 'none';
+      isCaps = true;
+    }
+  }
+}
+
+/* end caps click */
+
+/* start ctrl + alt click */
+
+function ctrlActive() {
+  const keyEng = document.querySelectorAll('.key__eng');
+  const keyRu = document.querySelectorAll('.key__ru');
+  if (localStorage.lang === 'en') {
+    for (let i = 0; i < keyRu.length; i += 1) {
+      keyRu[i].style.display = 'flex';
+      keyEng[i].style.display = 'none';
+      localStorage.lang = 'ru';
+    }
+  } else {
+    for (let i = 0; i < keyRu.length; i += 1) {
+      keyEng[i].style.display = 'flex';
+      keyRu[i].style.display = 'none';
+      localStorage.lang = 'en';
+    }
+  }
+}
+
+/* end ctrl + alt click */
+
+/* start switch key */
+
+function switchKeyDown(event) {
+  switch (event.isTrusted) {
+    case event.key === 'Shift' && isCaps:
+      shiftActive(event);
+      break;
+    case event.key === 'Shift' && !isCaps:
+      shiftNoActive(event);
+      break;
+    case event.key === 'CapsLock':
+      capsActive();
+      break;
+    case event.ctrlKey && event.altKey:
+      ctrlActive();
+      break;
+    default:
+  }
+}
+
+function switchKeyUp(event) {
+  switch (event.isTrusted) {
+    case event.key === 'Shift' && isCaps:
+      shiftNoActive(event);
+      break;
+    case event.key === 'Shift' && !isCaps:
+      shiftActive(event);
+      break;
+
+    default:
+  }
+}
+
+/* end switch key */
+
+window.addEventListener('keydown', (event) => {
+  addDataKey(event);
+  keyActive(event);
+  switchKeyDown(event);
+});
+
+window.addEventListener('keyup', (event) => {
+  keyNoActive(event);
+  switchKeyUp(event);
+});
+
+// window.addEventListener('mousedown', (event) => {
+//   console.log(event)
+// })
